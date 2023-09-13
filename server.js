f3008eb459193a2efd86b890191f197b24ed6ffe
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2023-09-13 12:40:34 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 8000;
+require('dotenv').config();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+// Routes
+const userRoutes = require('./routes/user/user');
+const tweetRoutes = require('./routes/tweet/tweet');
+app.use('/api/tweets', tweetRoutes);
+app.use('/api/users', userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
